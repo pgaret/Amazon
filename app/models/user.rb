a41c6ccd4cd48555b786cc22030b69bd52b448fc
pgaret <peregringaret@gmail.com>
2016-10-19 15:57:40 -0400

    Removed some lingering elements of the initial BCrypt setup for this branch

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,7 @@
 class User < ActiveRecord::Base
-  include BCrypt
+  
   has_many :reviews
   has_one :cart
   has_many :items, through: :cart
 
-  def password
-    @password ||= Password.new(password_hash)
-  end
-
-  def password=(new_password)
-    @password = Password.create(new_password)
-    self.password_hash = @password
 end
